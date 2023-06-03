24a7a94757345dab7e8f03d8b07fdb5d012f6859
Jacqueb-1337 <61178380+Jacqueb-1337@users.noreply.github.com>
2023-06-03 18:24:59 -0500

    Update main.js
    Lol I'm really bad at timings

--- src/js/main.js
+++ src/js/main.js
@@ -11,11 +11,11 @@
     const sr = window.sr = ScrollReveal()
 
     sr.reveal('.feature, .pricing-table-inner', {
-      duration: 1000,
+      duration: 1800,
       distance: '20px',
       easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
       origin: 'bottom',
-      interval: 100
+      interval: 500
     })
 
     doc.classList.add('anime-ready')
